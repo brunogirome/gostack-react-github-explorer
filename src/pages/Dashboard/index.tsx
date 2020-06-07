3a7d17b3f965d921dd063d485f584f6942ef4fe3
Bruno Girome <brunogirome@gmail.com>
2020-06-07 19:18:41 -0300

    Styles of Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImage from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logoImage} alt="Github Explorer" />
+      <Title>Explore Github repositories.</Title>
+
+      <Form action="">
+        <input placeholder="Insert the repository name" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars0.githubusercontent.com/u/44439996?s=460&u=ee7ce67dcf08207fca548118c5f4a4f3e66644d1&v=4"
+            alt="lnpc"
+          />
+          <div>
+            <strong>repostiory name</strong>
+            <p>A awesome text about the repository</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars0.githubusercontent.com/u/44439996?s=460&u=ee7ce67dcf08207fca548118c5f4a4f3e66644d1&v=4"
+            alt="lnpc"
+          />
+          <div>
+            <strong>repostiory name</strong>
+            <p>A awesome text about the repository</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars0.githubusercontent.com/u/44439996?s=460&u=ee7ce67dcf08207fca548118c5f4a4f3e66644d1&v=4"
+            alt="lnpc"
+          />
+          <div>
+            <strong>repostiory name</strong>
+            <p>A awesome text about the repository</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars0.githubusercontent.com/u/44439996?s=460&u=ee7ce67dcf08207fca548118c5f4a4f3e66644d1&v=4"
+            alt="lnpc"
+          />
+          <div>
+            <strong>repostiory name</strong>
+            <p>A awesome text about the repository</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
